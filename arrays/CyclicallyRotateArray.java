d1bd4f9631fa0865d11cee1c4be921c5feb9fcbd
akash-new <107805189+akash-new@users.noreply.github.com>
2022-06-21 14:26:49 +0530

    Create CyclicallyRotateArray.java

---
+++ arrays/CyclicallyRotateArray.java
@@ -0,0 +1,35 @@
+/*
+Approaches:
+
+1) Rotate once. Then do it k times. Time complexity is o(n * k)
+2) Rotate elements to the right. Store remaining elements that get pushed out in a separate array. Then finally add these elements into the original array
+3) Reverse two times approach. Reverse the two parts individually once. Then reverse the whole remaining array one more time to get the right answer. Same approach can be followed for anticlockwise rotation as well.
+Use this diagram for reference: https://github.com/Prince-1501/Hello_world-Competiitve-Programming/blob/master/Leetcode/Rotate%20Array%20Leetcode%20Solution.pdf
+
+Leetcode question: 
+
+Leetcode optimal solution:
+*/
+
+class Solution {
+    public void rotate(int[] arr, int k) {
+        int n = arr.length;
+        if(k>n) {
+            k = k%n;
+        }
+        reverseArray(arr, 0, n-k-1);
+        reverseArray(arr,  n-k, n-1);
+        reverseArray(arr, 0, n-1);
+    }
+    
+    public static void reverseArray(int arr[], int start, int end) {
+		
+		while(start<end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}
+}
