38b68c607685b09ebf76c0eb34333b4e2ba10722
akash-new <107805189+akash-new@users.noreply.github.com>
2022-06-21 21:09:27 +0530

    Update Remove DuplicatesfromSorted Array.java

--- arrays/Remove DuplicatesfromSorted Array.java
+++ arrays/Remove DuplicatesfromSorted Array.java
@@ -1,4 +1,6 @@
-/* Leetcode - https://leetcode.com/problems/remove-duplicates-from-sorted-array/  */
+/* Leetcode - https://leetcode.com/problems/remove-duplicates-from-sorted-array/ 
+Keep a pointer at the beginning and swap the number at the pointer with the new unique number found and increment the pointer ahead
+*/
 
 public int removeDuplicates(int[] nums) {
    int k =1;
